72b04af897b31faa290a3aa60c35de431c21a193
Jakub Mareda <jmareda@seznam.cz>
2020-03-14 14:26:02 +0100

    Add read file

---
+++ mareda-util/promises/fs/ReadFile.js
@@ -0,0 +1,20 @@
+﻿import * as fs from "fs";
+/**
+ *
+ * @param {import("fs").PathLike | string} filename
+ * @returns {Promise<Buffer>}
+ */
+function ReadFile(filename) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(filename, {}, function (err, res) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(res);
+            }
+        })
+    });
+}
+
+export default ReadFile;
