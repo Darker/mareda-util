ff2d77c4677e86df79833a38c8eca72424bde3d2
Jakub Mareda <jmareda@seznam.cz>
2020-03-14 13:07:37 +0100

    Add stuff from this pc

--- mareda-util/iterators/HelperGenerators.js
+++ mareda-util/iterators/HelperGenerators.js
@@ -16,6 +16,70 @@ function* toIterable(object) {
 }
 
 /**
+ * Returns iterator over items that were removed from the source array based on the predicate
+ * 
+ * @template T
+ * @param {T[]} data
+ * @param {function(T):boolean} predicate return true for items you want to remove
+ * @returns {IterableIterator<T>}
+ * 
+ * */
+function* spliced(data, predicate) {
+    for (let i = 0, l = data.length; i < l; ++i) {
+        const item = data[i];
+        if (predicate(item)) {
+            data.splice(i);
+            i--;
+            l--;
+            yield item;
+        }
+    }
+}
+
+/**
+ * @description Finds max value that is maximal according to the max calculator given in second argument
+ * @template T
+ * @param {Iterable<T>|T[]} iterable collection of items to search in
+ * @param {function(T):number} calculator function that translates value to number
+ * @param {boolean} returnIndex true if index should be returned instead of entry
+ * @returns {T|number} Index of the max item or -1
+ */
+function findMaxFunction(iterable, calculator, returnIndex = false, reverse = false) {
+    if (typeof calculator !== "function") {
+        throw new Error("findMaxIndex: Calculator must be given and must be function!");
+    }
+    let max = -Infinity;
+    let maxItemIndex = -1;
+    let maxItem = null;
+    const multiplier = reverse ? -1 : 1;
+    let index = 0;
+    for (const item of iterable) {
+        const itemValue = multiplier * calculator(item);
+        if (typeof itemValue !== "number") {
+            throw new Error("findMaxIndex: Calculator callback must return number!");
+        }
+        if (max < itemValue) {
+            max = itemValue;
+            maxItemIndex = index;
+            maxItem = item;
+        }
+        ++index;
+    }
+    return returnIndex ? maxItemIndex : maxItem;
+}
+/**
+ * @description Finds max value that is maximal according to the max calculator given in second argument
+ * @template T
+ * @param {Iterable<T>|T[]} iterable collection of items to search in
+ * @param {function(T):number} calculator function that translates value to number
+ * @returns {T|null} max item or null
+ */
+function findMax(iterable, calculator) {
+    return findMaxFunction(iterable, calculator, false, false);
+}
+
+
+/**
  * @template T
  * @param {Iterable<T>} iterableObject
  * @param {function(T):boolean} predicate
---
+++ mareda-util/iterators/RemovableIterator.js
@@ -0,0 +1,69 @@
+﻿/**
+ * @template T
+ * */
+class RemovableEntry {
+    /**
+     * 
+     * @param {T[]} array
+     * @param {number} index
+     */
+    constructor(array, index) {
+        this.entry = array[index];
+        this.array = array;
+        this.index = index;
+        this.removed = false;
+    }
+    remove() {
+        if (!this.removed) {
+            this.removed = true;
+            this.array.splice(this.index, 1);
+        }
+    }
+    get value() {
+        return this.array[this.index];
+    }
+    set value(value) {
+        this.replace(value);
+    }
+    /**
+     * Replace original item with new one. Cannot be used if item was deleted
+     * @param {T} newItem
+     */
+    replace(newItem) {
+        if (this.removed) {
+            throw new Error("RemovableEntry: cannot replace item that was deleted.");
+        }
+        this.entry = newItem;
+        this.array[this.index] = newItem;
+    }
+}
+
+/**
+ * @template T
+ * @param {T[]} array
+ * @returns {IterableIterator<RemovableEntry<T>>}
+ */
+function* RemovableIterator(array) {
+    for (let i = 0, l = array.length; i < l; ++i) {
+        const item = array[i];
+        const riter = new RemovableEntry(array, i);
+        yield riter;
+        if (riter.removed) {
+            --i;
+            --l;
+        }
+    }
+}
+
+//const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+//console.log(items);
+//for (const iter of removableIterator(items)) {
+//    if (iter.entry % 2 !== 0) {
+//        iter.remove();
+//    }
+//    else {
+//        iter.replace(iter.entry * 10);
+//    }
+//}
+//console.log(items);
+module.exports = RemovableIterator;
