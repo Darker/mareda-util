8c861a7fbdd0da2865cb0e0e0a703eb6b79b3924
Jakub Mareda <jmareda@seznam.cz>
2020-04-17 21:02:32 +0200

    Add write file

---
+++ mareda-util/promises/fs/WriteFile.js
@@ -0,0 +1,23 @@
+﻿import * as fs from "fs";
+
+/**
+ *
+ * @param {import("fs").PathLike | string} filename
+ * @param {string|Buffer} buffer
+ * @param {fs.WriteFileOptions} options
+ * @returns {Promise<void>}
+ */
+function WriteFile(filename, buffer, options) {
+    return new Promise(function (resolve, reject) {
+        fs.writeFile(filename, buffer, options, function (err) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve();
+            }
+        })
+    });
+}
+
+export default WriteFile;
