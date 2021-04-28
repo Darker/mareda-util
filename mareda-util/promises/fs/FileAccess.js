f64fd5b0bab98e8848ab293e4ab48aa1c7700054
Jakub Mareda <jmareda@seznam.cz>
2021-04-28 20:33:27 +0200

    added file access promise

---
+++ mareda-util/promises/fs/FileAccess.js
@@ -0,0 +1,20 @@
+﻿import * as fs from "fs";
+/**
+ *
+ * @param {string|Buffer|URL} name
+ * @param {number} mode
+ * @returns {Promise<boolean>}
+ */
+function FileAccess(name, mode = fs.constants.F_OK) {
+    return new Promise(function (resolve, reject) {
+        fs.access(name, mode, (err) => {
+            resolve(!err);
+        });
+    });
+}
+FileAccess.MODE = {
+    EXISTS: fs.constants.F_OK,
+    READ: fs.constants.R_OK,
+    WRITE: fs.constants.W_OK
+}
+export default FileAccess;
