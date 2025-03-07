6c3eb115ef1fed0a9adef9b9768e2d7151ebc54e
Jakub Mareda <jmareda@seznam.cz>
2025-03-07 08:20:38 +0100

    make timeout abortable

--- mareda-util/promises/timeoutPromise.js
+++ mareda-util/promises/timeoutPromise.js
@@ -1,5 +1,46 @@
-﻿function timeoutPromise(timeout) {
-    return new Promise(function (resolve) { setTimeout(resolve, timeout); });
+﻿
+/**
+ * 
+ * @param {number} timeout 
+ * @param {object} param1 
+ * @param {AbortSignal} [param1.abortSignal]
+ * @param {boolean} [param1.abortRejects] if true abort signal causes rejection
+ */
+function timeoutPromise(timeout, {abortSignal=null, abortRejects=true}={}) {
+    return new Promise(
+        function (resolve, reject) {
+            if(abortSignal && abortSignal.aborted) {
+                if(abortRejects) {
+                    reject(new Error("AbortError"));
+                }
+                else {
+                    resolve();
+                }
+                return;
+            }
+            let timeoutId;
+            function handleAbort() {
+                clearTimeout(timeoutId);
+                if(abortRejects) {
+                    reject(new Error("AbortError"));
+                }
+                else {
+                    resolve();
+                }
+            }
+            function handleSuccess() {
+                if(abortSignal) {
+                    abortSignal.removeEventListener("abort", handleAbort);
+                }
+                resolve();
+            }
+            timeoutId = setTimeout(handleSuccess, timeout);
+            if(abortSignal) {
+                abortSignal.addEventListener("abort", handleAbort, {once: true});
+            }
+            
+        }
+    );
 }
 
 export default timeoutPromise;
