2f1a15f715d13c3217720a0aade3564298a0c4b9
Jakub Mareda <jmareda@seznam.cz>
2026-02-23 13:56:38 +0100

    Add helper to convert hex strings to Uint8Array

---
+++ mareda-util/arrays/hex2bytes.js
@@ -0,0 +1,31 @@
+
+/** @type {{[hex: string]:number}} **/
+const hexToByte = {}
+
+for (let n = 0; n <= 0xff; ++n)
+{
+    const hexOctet = n.toString(16).padStart(2, "0");
+    hexToByte[hexOctet] = n;
+}
+
+Object.freeze(hexToByte);
+
+/**
+ * 
+ * @param {string} bytesHex 
+ * @returns 
+ */
+export default function hex2bytes(bytesHex)
+{
+    const len = bytesHex.length;
+    if(len%2 != 0) {
+        throw new Error("Hex string must have an even number of characters.");
+    }
+    const output = new Uint8Array(len/2);
+
+
+    for (let i = 0; i < len; i+=2) {
+        output[i/2] = hexToByte[bytesHex[i] + bytesHex[i+1]];
+    }
+    return output;
+}
